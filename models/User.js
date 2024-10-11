26a6947034e61965122279819b21d11dd227f976
Rafat <shaikhrafat25@gmail.com>
2024-10-11 15:25:16 +0530

    initial commit

---
+++ models/User.js
@@ -0,0 +1,27 @@
+// models/User.js
+
+const mongoose = require('mongoose');
+
+// Define the user schema
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Create and export the User model
+module.exports = mongoose.model('User', UserSchema);
